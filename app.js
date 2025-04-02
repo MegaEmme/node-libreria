6c4f3a6956572ef86907a6a645802c873e86061e
MegaEmme <mikmusco@gmail.com>
2025-04-02 13:26:38 +0200

    feat: add middleware logger

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const app = express();
 const port = 10000;
 
 const bookRouters = require('./routes/bookRoutes');
+const logger = require('./middlewares/logger');
 
 app.use(express.json());
+
+app.use("/", logger);
 
 app.use("/books", bookRouters);
 
--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,4 +1,7 @@
-function logger (req,res,next){
-};
-
+function logger(req, res, next) {
+    const { method, protocol, host, url } = req;
+    console.log(method, protocol + '://' + host + url);
+    next();
+  };
+  
 module.exports = logger;
